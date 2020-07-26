1a9a34111624ef7bb71d151fe1980a2e935f81f1
CleverNucleus <jamie@mclean007.plus.com>
2020-07-26 16:24:49 +0100

    Clean slate protocal
    We're changing master branch to represent 1.15.2 for now. All 1.12 code has been deleted.

--- src/main/java/clevernucleus/entitled/common/capability/ITag.java
+++
@@ -1,9 +0,0 @@
-package clevernucleus.entitled.common.capability;
-
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface ITag {
-	void set(NBTTagCompound par0);
-	
-	NBTTagCompound get();
-}
--- src/main/java/clevernucleus/entitled/common/util/IProxy.java
+++
@@ -1,9 +0,0 @@
-package clevernucleus.entitled.common.util;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public interface IProxy {
-	String I18n(String par0);
-	EntityPlayer getPlayer();
-}
