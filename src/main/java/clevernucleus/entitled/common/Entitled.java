f08f8587900750fc29b0c564e85560a5847eb4e3
CleverNucleus <jamie@mclean007.plus.com>
2020-09-08 14:13:28 +0100

    Refreshed Workspace for 1.16.2
    Oh boy here we go again!
    -Removed old code for 1.15.2
    +Set everything up for 1.16.2
    
    Forge is still in BETA for 1.16.2 so this may be tough!

--- src/main/java/clevernucleus/entitled/common/Entitled.java
+++ src/main/java/clevernucleus/entitled/common/Entitled.java
@@ -1,10 +1,5 @@
 package clevernucleus.entitled.common;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import clevernucleus.entitled.common.util.IProxy;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 /**
@@ -15,12 +10,6 @@ public class Entitled {
 	
 	/** The modid used to identify entitled. */
 	public static final String MODID = "entitled";
-	
-	/** Logger instance for tracking issues. */
-	public static final Logger LOGGER = LogManager.getLogger();
-	
-	/** Proxy instance to get side specific methods. */
-	public static final IProxy PROXY = DistExecutor.runForDist(() -> clevernucleus.entitled.client.ClientProxy::new, () -> clevernucleus.entitled.server.ServerProxy::new);
 	
 	public Entitled() {}
 }
